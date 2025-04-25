1b522fd4927508ef04f6bbbe99202ba1fdaa0748
danielgribanov <d.gribanov@carx-tech.com>
2025-04-25 10:13:19 +0300

    Багофикс

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor;
-using UnityEditor.Experimental.GraphView;
-using UnityEngine;
+// using UnityEditor;
+// using UnityEditor.Experimental.GraphView;
+ using UnityEngine;
 
 namespace Golf
 {
